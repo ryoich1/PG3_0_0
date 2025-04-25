e3d2a7b099934f40a21bea0cf4c7362d314a2d61
ryoich1 <k024g1007@g.neec.ac.jp>
2025-04-25 15:42:53 +0900

    01-02課題

--- main.cpp
+++ main.cpp
@@ -1,31 +1,27 @@
 #include<stdio.h>
 
-template<typename T>
-T Min(T a, T b) {
+int Recursive(int n,int m,int l,int a) {
 
-	if (a < b) {
-		return a;
+	if (n < m) {
+		printf("%d %d  %d\n", m, n, a);
+	} else if (n > m) {
+		printf("%d %d  %d\n", m, n, a);
+		return 1;
 	}
-	if (a > b) {
-		return b;
-	}
 
-	return 0;
-}
-
-template <>
-char Min<char>(char a, char b) {
-	printf("数字以外は代入できません");
-
-	return 0;
+	return (Recursive(n + n * 2 - 50, m + l, l, a + 1));
 }
 
 int main() {
+	
+	int l = 1072;//一般の時給
+	int m = 1072;//一般のトータル金
+	int n = 100;//再帰のトータル金
+	int a = 1;//再帰した回数
+	int result{};
 
-	printf("%d\n", Min<int>(1, 2));
-	printf("%f\n", Min<float>(1.0f, 2.0f));
-	printf("%lf\n", Min<double>(1.11000f, 2.22000f));
-	printf("%c\n", Min<char>('a', 'b'));
+	printf("一般 再帰 再帰回数\n");
+	result = Recursive(n, m, l, a);
 
 	return 0;
 }
