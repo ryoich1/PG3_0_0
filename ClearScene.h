7b700947f81f6243691ccd5647b0c6ef437ca6c8
ryoich1 <k024g1007@g.neec.ac.jp>
2025-07-18 14:27:29 +0900

    a

---
+++ ClearScene.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "IScene.h"
+
+//クリアシーン
+class ClearScene : public IScene {
+
+public:
+	void Initialize() override;
+	void Update(char* keys, char* preKeys) override;
+	void Draw() override;
+};
+
